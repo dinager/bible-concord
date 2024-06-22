2fb136b2b05635a061aef5a591983c660de44acd
dina <d.solomon@easysend.io>
2024-06-23 00:39:05 +0300

    fix __table_args__

--- server/db_model/word.py
+++ server/db_model/word.py
@@ -8,4 +8,4 @@ class Word(db.Model):
     value = db.Column(db.String(50), nullable=False, unique=True)
     length = db.Column(db.Integer, nullable=False)
 
-    __table_args__ = db.Index("word_value_idx", "value", unique=True)
+    __table_args__ = (db.Index("word_value_idx", "value", unique=True),)

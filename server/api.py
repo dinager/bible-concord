839369dd58d56969b4ccd13ccd97f741f2036c1c
dina <d.solomon@easysend.io>
2024-06-30 12:32:29 +0300

    add api mock

--- server/api.py
+++ server/api.py
@@ -3,6 +3,7 @@ from http import HTTPStatus
 
 from flask import Blueprint, Response, request
 
+from server.logic.api_mocks import MOCK_BOOKS, MOCK_BOOKS_NAMES, get_book_content_mock
 from server.logic.bible_book_parser import parse_book
 
 concord_blueprint = Blueprint(
@@ -21,17 +22,51 @@ def add_book() -> Response:
     """
     curl --location 'http://localhost:4200/api/add_book' --form 'textFile=@"/path/to/file.txt"' -F "bookName=genesis"
     """
+    # todo: use json schema validator
     if "textFile" not in request.files:
-        return Response(json.dumps({"error": "No file part"}), status=HTTPStatus.BAD_REQUEST)
+        return Response("No file part", status=HTTPStatus.BAD_REQUEST)
     if "bookName" not in request.form:
-        return Response(json.dumps({"error": "No book name"}), status=HTTPStatus.BAD_REQUEST)
+        return Response("No book name", status=HTTPStatus.BAD_REQUEST)
+    # if "division" not in request.form:
+    #     return Response("No division", status=HTTPStatus.BAD_REQUEST)
     book_name = request.form.get("bookName")
     # Assuming the file is in the following format: tests/resources/genesis.txt
     file = request.files["textFile"]
     book_text = file.read().decode("utf-8")
     parsed_book = parse_book(book_name, book_text)
     return Response(
-        response=f"received book with {parsed_book.num_chapters} chapters",
+        f"received book with {parsed_book.num_chapters} chapters",
+        status=HTTPStatus.OK,
+        mimetype="text/html",
+    )
+
+
+@concord_blueprint.route("/api/get_books", methods=["GET"])
+def get_books() -> Response:
+    """
+    curl 'http://localhost:4200/api/get_books'
+    """
+    return Response(
+        json.dumps({"books": MOCK_BOOKS}),
+        status=HTTPStatus.OK,
+        mimetype="application/json",
+    )
+
+
+@concord_blueprint.route("/api/get_book_content/<book_name>", methods=["GET"])
+def get_book_content(book_name: str) -> Response:
+    """
+    curl 'http://localhost:4200/api/get_book_content/Genesis'
+    """
+    if book_name.lower() not in MOCK_BOOKS_NAMES:
+        return Response(
+            f"book {book_name} not found",
+            status=HTTPStatus.NOT_FOUND,
+            mimetype="text/html",
+        )
+    book_content = get_book_content_mock(book_name)
+    return Response(
+        book_content,
         status=HTTPStatus.OK,
         mimetype="text/html",
     )
---
+++ server/logic/api_mocks.py
@@ -0,0 +1,32 @@
+"""
+This file should contains mock responses for the API endpoints.
+Once endpoint is implemented remove un relevant methods
+
+"""
+import os
+
+ROOT_PATH = os.path.join(os.path.dirname(__file__), "..", "..")
+
+MOCK_BOOKS = [
+    {
+        "name": "Genesis",
+        "division": "Torah",
+        "insertTime": "2024-30-06 10:55",
+    },
+    {
+        "name": "Exodus",
+        "division": "Neviim",
+        "insertTime": "2024-01-07 11:01",
+    },
+]
+
+MOCK_BOOKS_NAMES = ["genesis", "exodus"]
+
+
+def get_book_content_mock(book_name: str) -> str | None:
+    test_resources_path = os.path.join(ROOT_PATH, "tests", "resources")
+    book_name = book_name.lower()
+    if book_name not in ["genesis", "exodus"]:
+        return None
+    with open(os.path.join(test_resources_path, f"{book_name}.txt"), "r") as file:
+        return file.read()

a2ccc9272608caa2238175543ee6433bbd069bc3
dina <d.solomon@easysend.io>
2024-08-22 15:19:40 +0300

    add scroll for books

--- client/src/components/Books.js
+++ client/src/components/Books.js
@@ -40,41 +40,43 @@ const Books = () => {
         <div>
             <h1>Books</h1>
             <button onClick={() => navigate(`/add-book`)}>Add Book</button>
-            <table>
-                <thead>
-                <tr>
-                    <th>Book Name</th>
-                    <th>Division</th>
-                    <th>Insert Time</th>
-                    <th>Actions</th>
-                </tr>
-                </thead>
-                <tbody>
-                {books.map((book) => (
-                    <tr key={book.name} onClick={() => handleRowClick(book.name)}>
-                        <td style={{ textTransform: 'capitalize' }}>{book.name}</td>
-                        <td style={{ textTransform: 'capitalize' }}>{book.division}</td>
-                        <td>{book.insertTime}</td>
-                        <td>
-                            <button 
-                                type="button" 
-                                onClick={() => handleRowClick(book.name)}
-                                className="button-spacing button-green"
-                            >
-                                Show Content
-                            </button>
-                            <button 
-                                type="button" 
-                                onClick={(e) => { e.stopPropagation(); handleDelete(book.name); }}
-                                className="button-delete"
-                            >
-                                Delete Book
-                            </button>
-                        </td>
+            <div style={{maxHeight: '700px', overflowY: 'auto', border: '1px solid #ccc'}}>
+                <table style={{width: '100%', borderCollapse: 'collapse'}}>
+                    <thead>
+                    <tr>
+                        <th>Book Name</th>
+                        <th>Division</th>
+                        <th>Insert Time</th>
+                        <th>Actions</th>
                     </tr>
-                ))}
-                </tbody>
-            </table>
+                    </thead>
+                    <tbody>
+                    {books.map((book) => (
+                        <tr key={book.name} onClick={() => handleRowClick(book.name)}>
+                            <td style={{ textTransform: 'capitalize'}}>{book.name}</td>
+                            <td style={{ textTransform: 'capitalize'}}>{book.division}</td>
+                            <td>{book.insertTime}</td>
+                            <td>
+                                <button
+                                    type="button"
+                                    onClick={() => handleRowClick(book.name)}
+                                    className="button-spacing button-green"
+                                >
+                                    Show Content
+                                </button>
+                                <button
+                                    type="button"
+                                    onClick={(e) => {e.stopPropagation();handleDelete(book.name);}}
+                                    className="button-delete"
+                                >
+                                    Delete Book
+                                </button>
+                            </td>
+                        </tr>
+                    ))}
+                    </tbody>
+                </table>
+            </div>
         </div>
     );
 };
